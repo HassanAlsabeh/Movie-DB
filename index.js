9d613b3af35584b93b71e0680620bb2340d6ed55
HassanAlsabeh <sabehf17@gmail.com>
2021-06-14 13:05:34 +0300

    Step 3

--- index.js
+++ index.js
@@ -7,3 +7,38 @@ app.get("/",(req,res)=>{
 })
 
 app.listen(3000); 
+
+
+app.get('/test',(req,res) => {
+    const test = req.query.s;
+
+    if (typeof test != 'undefined') {
+        // test string applied
+        const response = {
+            status:200, message:"ok", data: test
+        };
+
+        res.send(response);
+    }
+    else {
+        const response = {
+            status:200, message: "OK"
+        };
+
+
+        res.status(200);
+        res.send(response);
+    }
+    
+}
+
+
+);
+
+app.get('/time',(req,res) =>{
+    let date_ob = new Date();
+    let time = date_ob.getHours() + ":" + date_ob.getMinutes();
+    let response = { status:200, message: time };
+    res.send(response);
+}
+)
